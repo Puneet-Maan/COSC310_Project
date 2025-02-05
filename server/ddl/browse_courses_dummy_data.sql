37a5d99efd3a61fbca40f89d165305d0faf5f9dc
vernie panagsagan <verniepanagsagan@vernies-MacBook-Pro.local>
2025-02-04 21:09:21 -0800

    adding dummy data for browse courses

---
+++ server/ddl/browse_courses_dummy_data.sql
@@ -0,0 +1,24 @@
+
+INSERT INTO courses (course_code, course_name, department, credits, requires_lab) VALUES
+('BIOL116','Biology for Science Majors I','Science',3,TRUE),
+('BIOL125','Biology for Science Majors II','Science',3,TRUE),
+('CHEM121','Atomic and Molecular Chemistry','Science',3,TRUE),
+('CHEM123','Physical and Organic Chemistry','Science',3,TRUE),
+('COSC111','Computer Programming I','Science',3,TRUE),
+('COSC121', 'Computer Programing II', 'Science', 3, TRUE),
+('COSC123','Computer Creativity', 'Science',3,TRUE),
+('COSC211','Machine Architecture','Science',3,FALSE),
+('COSC222','Data Structures','Science',3,TRUE),
+('COSC221','Discrete Structures in Computing','Science', 3, TRUE),
+('COSC304','Introduction to Databases','Science', 3, TRUE),
+('COSC305','Project Management','Science',3,FALSE),
+('COSC310','Software Engineering','Science',3,FALSE),
+('COSC320','Analysis of Algorithms','Science',3,FALSE),
+('COSC341','Human Computer Interaction','Science',3,FALSE),
+('COSC360','Web Programming','Science',3,TRUE),
+('COSC407','Introduction to Parallel Computing','Science',3,TRUE),
+('DATA101','Making Predictions with Data','Science',3,TRUE),
+('DATA301','Introduction to Data Analystics','Science',3,TRUE),
+('DATA311','Machine Learning','Science',3,TRUE)
+
+
